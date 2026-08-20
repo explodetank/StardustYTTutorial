01e2fe5ac3209d4a485468d72668cfebc07c1484
explodetank <explodetank@gmail.com>
2026-08-19 21:44:40 -0700

    Removed placeholder sprite. Added prototype clicker class and scene.

---
+++ Scenes/Prototype/Clicker/prototype_clicker.gd
@@ -0,0 +1,32 @@
+class_name PrototypeClicker
+extends Control
+## A clicker prototype creating stardust.
+
+# How to document a class.
+# Use double #
+
+## A reference to the label displaying the current amount of stardust created.
+@export var label:Label
+
+## The amount of stardust created.
+var stardust:int = 0
+
+# Runs when this node is created.
+## Initializes the label on launch.
+func _ready() -> void:
+	update_label_text()
+
+# Better to isolate this function rather than putting it in
+# the button pressed function just so we know where things are.
+## Adds one stardust.
+func create_stardust() -> void:
+	stardust += 1
+	update_label_text()
+
+## Refresh the label to display the current amount of stardust.
+func update_label_text() -> void:
+	label.text = "Stardust : %s" %stardust
+
+## Triggered when the create stardust button is pressed.
+func _on_button_pressed() -> void:
+	create_stardust()
